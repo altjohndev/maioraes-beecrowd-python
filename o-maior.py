b4e9ec4a0b5c450f3a52c5ded0e55ff60ce32d2f
Adriano Moraes <maioraes@gmail.com>
2022-05-18 20:03:00 +0300

    Correção de o maior

--- o-maior.py
+++ o-maior.py
@@ -3,7 +3,7 @@ a = int(line[0])
 b = int(line[1])
 c = int(line[2])
 
-x = (a + b + abs (a - b)) / 2
-y = (x + c + abs (x - c)) / 2
+x = (a + b + abs(a - b)) / 2
+y = (x + c + abs(x - c)) / 2
 
 print(f'{y:.0f} eh o maior')
