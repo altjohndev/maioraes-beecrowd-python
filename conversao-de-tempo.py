d9d212ebc3087596ec19b47fde879fed219232e3
Adriano Moraes <maioraes@gmail.com>
2022-05-19 23:42:54 +0300

    Correção de Conversão de Tempo

--- conversao-de-tempo.py
+++ conversao-de-tempo.py
@@ -1,11 +1,11 @@
 segundos = int(input())
 
-hora = int(segundos // 3600)
+hora = (segundos // 3600)
 segundos = segundos % 3600
 
-minutos = int(segundos // 60)
+minutos = (segundos // 60)
 segundos = segundos % 60
 
-segundos = int(segundos // 1)
+segundos = (segundos)
 
 print(f'{hora}:{minutos}:{segundos}')
