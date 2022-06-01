9be60d0e7115e4db6f64b99c4df0685352651e56
Adriano Moraes <maioraes@gmail.com>
2022-06-01 17:53:42 +0300

    Solucão de Intervalo

--- intervalo.py
+++ intervalo.py
@@ -1,16 +1,12 @@
 valor = float(input())
 
-if valor >= 0.00 and valor <= 25.00:
-  print(f'Intervalo [0,25]')
-
-elif valor > 25.00 and valor <= 50.00:
-  print(f'Intervalo (25,50]')
-
-elif valor > 50.00 and valor <= 75.00:
-  print(f'Intervalo (50,75]')
-
-elif valor > 75.00 and valor <= 100.00:
-  print(f'Intervalo (75,100]')
-
+if valor >= 0 and valor <= 25:
+  print('Intervalo [0,25]')
+elif valor > 25 and valor <= 50:
+  print('Intervalo (25,50]')
+elif valor > 50 and valor <= 75:
+  print('Intervalo (50,75]')
+elif valor > 75 and valor <= 100:
+  print('Intervalo (75,100]')
 else:
   print('Fora de intervalo')
