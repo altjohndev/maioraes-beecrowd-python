f6ca8039085a263a1b46e334b8909d1457509534
Adriano Moraes <maioraes@gmail.com>
2022-06-13 04:15:35 +0300

    Solução de Múltiplos

---
+++ multiplos.py
@@ -0,0 +1,17 @@
+valor = input().split()
+
+a = int(valor[0])
+b = int(valor[1])
+
+if a == b:
+    print('Sao Multiplos')
+if (a > b):
+    if (a % b) == 0:
+     print('Sao Multiplos')
+    else:
+      print('Nao sao Multiplos')
+if (b > a):
+    if (b % a) == 0:
+      print('Sao Multiplos')
+    else:
+      print('Nao sao Multiplos')
